02b2ae19389abac71061665d2c6aa6d1b3b2d495
NicholasBatesNZ <19882546+NicholasBatesNZ@users.noreply.github.com>
2023-08-05 09:49:47 +1200

    improvements, setup codebuild, gui for upload

--- main.py
+++ main.py
@@ -1,41 +1,84 @@
+import glob
+import threading
+import time
 import tkinter as tk
 from tkinter import ttk
-import threading
 
 from prepare_server import perpare_server
 
-def do_the_thing(version):
-    loading_spinner.config(mode="indeterminate")
-    loading_spinner.start()
 
-    button.config(state=tk.DISABLED)
+def prepare(version):
+    prepare_loading_spinner.config(mode="indeterminate")
+    prepare_loading_spinner.start()
+
+    prepare_button.config(state=tk.DISABLED)
     try:
-        perpare_server(version)
+        path = perpare_server(version)
     except:
         pass
-    button.config(state=tk.NORMAL)
+    prepare_button.config(state=tk.NORMAL)
+    folder_entry.set_menu(*glob.glob('tmp_*'))
+    folder_value.set(path)
+    upload_button.config(state=tk.NORMAL)
 
-    loading_spinner.stop()
-    loading_spinner.config(mode="determinate")
+    prepare_loading_spinner.stop()
+    prepare_loading_spinner.config(mode="determinate")
 
-def on_button_click():
-    thread = threading.Thread(target=do_the_thing, args=(text_entry.get(),))
+def upload(tag, folder):
+    upload_loading_spinner.config(mode="indeterminate")
+    upload_loading_spinner.start()
+
+    upload_button.config(state=tk.DISABLED)
+    try:
+        print(tag)
+        print(folder)
+        time.sleep(3)
+    except:
+        pass
+    upload_button.config(state=tk.NORMAL)
+
+    upload_loading_spinner.stop()
+    upload_loading_spinner.config(mode="determinate")
+
+def on_prepare_button_click():
+    thread = threading.Thread(target=prepare, args=(version_entry.get(),))
+    thread.start()
+
+def on_upload_button_click():
+    thread = threading.Thread(target=upload, args=(tag_entry.get(), folder_value.get()))
     thread.start()
 
 # Create the main application window
 root = tk.Tk()
 root.title("Magic Minecrafterer!")
 
-label = tk.Label(root, text="Minecraft version:")
-text_entry = tk.Entry(root)
-button = tk.Button(root, text="Download", command=on_button_click)
-loading_spinner = ttk.Progressbar(root, mode="determinate", value=0)
+version_label = tk.Label(root, text="Minecraft version:")
+version_entry = tk.Entry(root)
+prepare_button = tk.Button(root, text="Download", command=on_prepare_button_click)
+prepare_loading_spinner = ttk.Progressbar(root, mode="determinate", value=0)
+
+tag_label = tk.Label(root, text="Tag (MUST be unique or will overwrite):")
+tag_entry = tk.Entry(root)
+folder_label = tk.Label(root, text="Folder:")
+folder_value = tk.StringVar(root)
+folder_options = glob.glob('tmp_*')
+folder_value.set("No folders found" if len(folder_options) == 0 else folder_options[0])
+folder_entry = ttk.OptionMenu(root, folder_value, *folder_options)
+upload_button = tk.Button(root, text="Upload", command=on_upload_button_click, state=tk.DISABLED if len(folder_options) == 0 else tk.NORMAL)
+upload_loading_spinner = ttk.Progressbar(root, mode="determinate", value=0)
 
 # Grid layout for widgets
-label.grid(row=0, column=0, padx=10, pady=10)
-text_entry.grid(row=0, column=1, padx=10, pady=10)
-button.grid(row=1, column=0, columnspan=2, padx=10, pady=10)
-loading_spinner.grid(row=2, column=0, columnspan=2, padx=10, pady=10)
+version_label.grid(row=0, column=0, padx=10, pady=10)
+version_entry.grid(row=0, column=1, padx=10, pady=10)
+prepare_button.grid(row=1, column=0, columnspan=2, padx=10, pady=10)
+prepare_loading_spinner.grid(row=2, column=0, columnspan=2, padx=10, pady=10)
+
+tag_label.grid(row=3, column=0, padx=10, pady=10)
+tag_entry.grid(row=3, column=1, padx=10, pady=10)
+folder_label.grid(row=4, column=0, padx=10, pady=10)
+folder_entry.grid(row=4, column=1, padx=10, pady=10)
+upload_button.grid(row=5, column=0, columnspan=2, padx=10, pady=10)
+upload_loading_spinner.grid(row=6, column=0, columnspan=2, padx=10, pady=10)
 
 # Start the main event loop
 root.mainloop()
--- prepare_server.py
+++ prepare_server.py
@@ -14,7 +14,7 @@ def perpare_server(version):
     download_url = f'https://api.papermc.io/v2/projects/paper/versions/{version}/builds/{number}/downloads/{name}'
     print(f'Downloading jar from {download_url}')
 
-    path = f'tmp_{random.getrandbits(128)}'
+    path = f'tmp_{version}_{random.getrandbits(32)}'
     print(f'Server generating in {path}')
 
     os.mkdir(path)
@@ -23,12 +23,15 @@ def perpare_server(version):
     open(f'{path}/eula.txt', 'w').write('eula=true')
 
     os.system(f'cp Dockerfile {path}/Dockerfile')
+    os.system(f'cp buildspec.yml {path}/buildspec.yml')
     os.system(f'cp ops.json {path}/ops.json')
     os.system(f'cp server.properties {path}/server.properties')
 
     ssm = boto3.client('ssm')
     password = ssm.get_parameter(Name='mc-rcon-password', WithDecryption=True)['Parameter']['Value']
     open(f'{path}/server.properties', 'a').write(f'rcon.password={password}')
+
+    return path
 
 if __name__ == "__main__":
     perpare_server("1.20.1" if len(sys.argv) == 1 else sys.argv[1])
