5dd5356dabda09954f5c1e35c31f04e0b66f4e3c
NZKea <20099289+NZKea@users.noreply.github.com>
2023-10-30 20:33:16 +1300

    Adding version info to frontend

--- manageServer/main.js
+++ manageServer/main.js
@@ -81,19 +81,24 @@ const taskFamilies = [...new Set(
   data.ResourceTagMappingList.map(
     (resource) => {
       const [prefix, suffix] = resource.ResourceARN.split('/');
-      return `${prefix}/${suffix.split(':')[0]}`;
+      const version = resource.Tags.filter(tag => tag.Key === "McVersion")[0];
+      return {
+          arn: `${prefix}/${suffix.split(':')[0]}`,
+          version: version?.Value ?? "1.?.?"
+      }
     },
   ),
 )];
 
-const tasks = taskFamilies.map((family) => ({
-  arn: family,
-  tag: family.split('/')[1],
+const tasks = taskFamilies.map(({arn, version}) => ({
+  arn,
+  version,
+  tag: arn.split('/')[1]
 }));
 
 tasks.forEach((task) => {
   const item = document.createElement('modus-list-item');
-  item.appendChild(document.createTextNode(task.tag));
+  item.appendChild(document.createTextNode(`${task.tag} | ${task?.version}`));
   item.onclick = () => { state.selectedTask = task; };
   document.querySelector('#taskList').appendChild(item);
 });
