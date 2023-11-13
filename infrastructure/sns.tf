9b175940f28e89082595ca67535c940eb06d630a
NicholasBatesNZ <19882546+NicholasBatesNZ@users.noreply.github.com>
2023-11-13 20:09:11 +1300

    fix

--- infrastructure/sns.tf
+++ infrastructure/sns.tf
@@ -14,7 +14,7 @@ resource "aws_sns_topic_subscription" "events_subscription_discord" {
   topic_arn = aws_sns_topic.server_events_topic.arn
 }
 
-resource "aws_sns_topic_subscription" "events_subscription_route53" {
+resource "aws_sns_topic_subscription" "scaling_subscription_route53" {
   endpoint  = aws_lambda_function.manage_route53_record.arn
   protocol  = "lambda"
   topic_arn = aws_sns_topic.scaling_events_topic.arn
