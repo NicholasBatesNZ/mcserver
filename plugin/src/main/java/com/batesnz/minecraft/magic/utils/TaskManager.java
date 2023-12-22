11f47b2f290e81200b91d8d9ac83c298f75aecce
NicholasBatesNZ <19882546+NicholasBatesNZ@users.noreply.github.com>
2023-12-23 11:25:19 +1300

    some more code

--- plugin/src/main/java/com/batesnz/minecraft/magic/utils/TaskManager.java
+++ plugin/src/main/java/com/batesnz/minecraft/magic/utils/TaskManager.java
@@ -1,5 +1,11 @@
 package com.batesnz.minecraft.magic.utils;
 
+import com.batesnz.minecraft.magic.Magic;
+import net.kyori.adventure.text.Component;
+import software.amazon.awssdk.core.SdkBytes;
+import software.amazon.awssdk.services.lambda.LambdaClient;
+import software.amazon.awssdk.services.lambda.model.InvocationType;
+import software.amazon.awssdk.services.lambda.model.InvokeRequest;
 import software.amazon.awssdk.services.resourcegroupstaggingapi.ResourceGroupsTaggingApiClient;
 import software.amazon.awssdk.services.resourcegroupstaggingapi.model.GetResourcesRequest;
 import software.amazon.awssdk.services.resourcegroupstaggingapi.model.ResourceTagMapping;
@@ -67,7 +73,18 @@ public class TaskManager {
 
         if (resource.isEmpty()) return false;
 
-        System.out.println("hi");
+        try (var lambda = LambdaClient.create()) {
+            var request = InvokeRequest.builder()
+                    .functionName("RunTask")
+                    .payload(SdkBytes.fromUtf8String("{\"taskDefinition\":\"" + resource.get().resourceARN() + "\"}"))
+                    .invocationType(InvocationType.EVENT)
+                    .build();
+
+            lambda.invoke(request);
+        } catch (Exception e) {
+            Magic.logger.error((Component) e);
+            return false;
+        }
 
         return true;
     }
